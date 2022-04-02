5bf72f2ecf95649a868cee8b613e5663db8746d5
erik.masny <erik.masny@gmail.com>
2022-04-02 17:50:18 +0200

    chore: init coreui template

--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-const app = createApp(App);
+import CoreuiVue from '@coreui/vue'
+import CIcon from '@coreui/icons-vue'
+import { iconsSet as icons } from '@/assets/icons'
+import DocsCallout from '@/components/DocsCallout'
+import DocsExample from '@/components/DocsExample'
 
-app.use(router);
+const app = createApp(App)
+app.use(store)
+app.use(router)
+app.use(CoreuiVue)
+app.provide('icons', icons)
+app.component('CIcon', CIcon)
+app.component('DocsCallout', DocsCallout)
+app.component('DocsExample', DocsExample)
 
-app.mount("#app");
+app.mount('#app')
