c52080a974c33157fb36bf1a0ad128735d94eaba
erik.masny <erik.masny@gmail.com>
2022-04-10 12:55:54 +0200

    build: disable prettier causing errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    'plugin:prettier/recommended',
+    // 'plugin:prettier/recommended',
   ],
   parserOptions: {
     parser: '@babel/eslint-parser',
@@ -15,6 +15,9 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/multi-word-component-names': 'off',
+      // "prettier/prettier": ["error",{
+      //   "endOfLine": "auto"}
+      // ]
   },
   overrides: [
     {
