f7e163047f5910e2bc91277365a19aed57aa13f7
mareklunter <marek.lunter02@gmail.com>
2022-05-04 21:54:38 +0200

    fix: rename users to students

---
+++ src/views/students/Create.vue
@@ -0,0 +1,166 @@
+<template>
+    <div>
+        <CCard class="mb-4">
+            <CCardBody>
+                <CForm @submit.prevent="submitForm">
+                    <h2 class="mb-3 text-center">Študíjne informácie</h2>
+                    <CRow>
+                        <CCol xs>
+                            <div class="mb-3">
+                                <CFormLabel for="faculty">Fakulta</CFormLabel>
+                                <CFormSelect
+                                aria-label="Default select example"
+                                :options="[
+                                    'Vyberte fakultu',
+                                    { label: 'FEI', value: 'fei' },
+                                    { label: 'FIIT', value: 'fiit' },
+                                    { label: 'SJF', value: '3', disabled: true }
+                                ]">
+                                </CFormSelect>
+                            </div>
+                        </CCol>
+                        <CCol xs>
+                            <div class="mb-3">
+                                <CFormLabel for="study_type">Stupeň štúdia</CFormLabel>
+                                <CFormSelect
+                                aria-label="Default select example"
+                                :options="[
+                                    'Vyberte stupeň štúdia',
+                                    { label: 'Bc', value: 'fei' },
+                                    { label: 'Ing', value: 'fiit' },
+                                    { label: 'PhD', value: '3' }
+                                ]">
+                                </CFormSelect>
+                            </div>
+                        </CCol>
+                        <CCol xs>
+                            <div class="mb-3">
+                                <CFormLabel for="programme">Odbor</CFormLabel>
+                                <CFormSelect
+                                aria-label="Default select example"
+                                :options="[
+                                    'Vyberte odbor',
+                                    { label: 'Aplikovaná informatika', value: 'api' },
+                                    { label: 'Mechatronika', value: 'mech' },
+                                ]">
+                                </CFormSelect>
+                            </div>
+                        </CCol>
+                    </CRow>
+                    <CRow>
+                        <div class="mb-3">
+                            <CFormLabel for="avatar">Avatar</CFormLabel>
+                            <CFormInput type="file" v-model="student.avatar" id="avatar" />
+                        </div>
+                    </CRow>
+
+                    <hr class="my-5">
+
+                    <h2 class="mb-3 text-center">Osobné údaje</h2>
+                    <CRow>
+                        <CCol xs>
+                            <div class="mb-3">
+                                <CFormLabel for="name">Meno</CFormLabel>
+                                <CFormInput type="text" v-model="student.name" id="name" />
+                            </div>
+                        </CCol>
+                        <CCol xs>
+                            <div class="mb-3">
+                                <CFormLabel for="surname">Priezvisko</CFormLabel>
+                                <CFormInput type="text" v-model="student.surname" id="surname" />
+                            </div>
+                        </CCol>
+                    </CRow>
+                    <CRow>
+                        <CCol xs>
+                            <div class="mb-3">
+                                <CFormLabel for="name">Email</CFormLabel>
+                                <CFormInput type="email" v-model="student.email" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                            </div>
+                        </CCol>
+                        <CCol xs>
+                            <div class="mb-3">
+                                <CFormLabel for="privateEmail">Súkromný email</CFormLabel>
+                                <CFormInput type="text" v-model="student.privateEmail" id="privateEmail" />
+                            </div>
+                        </CCol>
+                    </CRow>
+
+                    <hr class="my-5">
+
+                    <h2 class="text-center my-3">Adresa</h2>
+                    <CRow>
+                        <CCol xs>
+                            <div class="mb-3">
+                                <CFormLabel for="street">Ulica</CFormLabel>
+                                <CFormInput type="text" v-model="student.address.street" id="street" />
+                            </div>
+                        </CCol>
+                        <CCol xs>
+                            <div class="mb-3">
+                                <CFormLabel for="houseNumber">Číslo domu</CFormLabel>
+                                <CFormInput type="text" v-model="student.address.houseNumber" id="houseNumber" />
+                            </div>
+                        </CCol>
+                    </CRow>
+                    <CRow>
+                        <CCol xs>
+                            <div class="mb-3">
+                                <CFormLabel for="city">Mesto</CFormLabel>
+                                <CFormInput type="text" v-model="student.address.city" id="city" />
+                            </div>
+                        </CCol>
+                        <CCol xs>
+                            <div class="mb-3">
+                                <CFormLabel for="postcode">PSČ</CFormLabel>
+                                <CFormInput type="text" v-model="student.address.postcode" id="postcode" />
+                            </div>
+                        </CCol>
+                    </CRow>
+                    <CRow>
+                        <CCol xs>
+                            <div class="mb-3">
+                                <CFormLabel for="country">Štát</CFormLabel>
+                                <CFormInput type="text" v-model="student.address.country" id="country" />
+                            </div>
+                        </CCol>
+                    </CRow>
+                
+                    <div class="d-grid">
+                        <CButton type="submit" color="primary">Uložiť</CButton>
+                    </div>
+                </CForm>
+            </CCardBody>
+        </CCard>
+    </div>
+</template>
+
+<script>
+import {ref} from 'vue'
+
+export default {
+    name: 'Students.create',
+    setup() {
+        const student = ref({
+            name: '',
+            surname: '',
+            email: '',
+            privateEmail: '',
+            address: {
+                street: '',
+                houseNumber: '',
+            }
+        })
+
+        const submitForm = function() {
+            console.log(student)
+            alert('submit')
+        }
+
+        return {
+            student,
+            submitForm
+        }
+    }
+}
+</script>
---
+++ src/views/students/Show.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <h1>Profil Študenta</h1>
+    <CCard>
+      <CCardBody>
+        <CRow>
+          <CCol md="4">
+            <img src="http://s3.amazonaws.com/37assets/svn/765-default-avatar.png" alt="">
+            <!-- <CAvatar
+              size="xl"
+              :src="student.avatar.src"
+              :status="student.avatar.status"
+            /> -->
+            <h2>{{ `${student.firstName} ${student.lastName}` }}</h2>
+            <p><strong>ID:</strong> {{ student.uniqueId }}</p>
+          </CCol>
+
+          <CCol md="8">
+            <CCardTitle> Osobné údaje </CCardTitle>
+            <CCardText> <strong>Meno:</strong> {{ `${student.firstName} ${student.lastName}` }} </CCardText>
+            <CCardText> <strong>Email:</strong> {{ student.email }} </CCardText>
+            <CCardText>
+              <strong>Sukromný email:</strong> {{ student.privateEmail }}
+            </CCardText>
+            <hr>
+            <CCardTitle> Adresa </CCardTitle>
+            <CCardText>
+              <strong>Krajina:</strong> {{ student.adress.country }}
+            </CCardText>
+            <CCardText>
+              <strong>Mesto:</strong> {{ student.adress.city }}
+            </CCardText>
+            <CCardText>
+              <strong>PSČ:</strong> {{ student.adress.postalCode }}
+            </CCardText>
+            <CCardText>
+              <strong>Ulica a číslo:</strong> {{ `${student.adress.street} ${student.adress.houseNumber}` }}
+            </CCardText>
+            <hr />
+            <CCardTitle> Štúdium </CCardTitle>
+            <CCardText>
+              <strong>Fakulta:</strong> {{ student.study.faculty }}
+            </CCardText>
+            <CCardText>
+              <strong>Odbor:</strong> {{ student.study.programme }}
+            </CCardText>
+            <CCardText>
+              <strong>Forma:</strong> {{ student.study.form }}
+            </CCardText>
+            <CCardText>
+              <strong>Titul:</strong> {{ student.academicDegree }}
+            </CCardText>
+          </CCol>
+        </CRow>
+      </CCardBody>
+    </CCard>
+  </div>
+</template>
+
+<script>
+import avatar1 from '@/assets/images/avatars/1.jpg'
+
+export default {
+  name: 'Students.show',
+  setup() {
+    return {
+      student: {
+        avatar: { src: avatar1, status: 'success' },
+        firstName: 'Yiorgos',
+        lastName: 'Avraamu',
+        email: 'xibrahim@stuba.sk',
+        privateEmail: 'ibrahim@gmail.com',
+        uniqueId: '123456',
+        academicDegree: 'Bc.',
+        study: {
+          faculty: 'FEI',
+          programme: 'Aplikovaná informatika',
+          form: 'Denná',
+        },
+        adress: {
+          street: 'Moskovska',
+          houseNumber: '23',
+          city: 'Zilina',
+          postalCode: '50648',
+          country: 'Slovensko',
+        },
+      },
+    }
+  },
+}
+</script>
